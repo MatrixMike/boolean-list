084154ab7b3e1cf090f9252eb0b9a39204ed9793
alan hawkins <hawk.alan@gmail.com>
2014-06-14 13:31:55 +1000

    added convinience methods

--- Data/BooleanList.hs
+++ Data/BooleanList.hs
@@ -9,23 +9,31 @@ integerToBooleanList 0 = []
 integerToBooleanList n = integerToBooleanList div ++ [toEnum (fromIntegral rem)]
   where (div,rem) = divMod n 2
 
-booleanListToInteger :: [Bool] -> Int
-booleanListToInteger (x:[]) = fromEnum x
-booleanListToInteger (x:xs) = ((2 * fromEnum x) ^ (length xs)) + rest
+booleanListToInteger :: Integral a => [Bool] -> a
+booleanListToInteger (x:[]) = fromIntegral (fromEnum x)
+booleanListToInteger (x:xs) = ((2 * fromIntegral (fromEnum x)) ^ (length xs)) + rest
   where rest = booleanListToInteger xs
 booleanListToInteger [] = 0
 
 takeIntegerFromBooleanList length xs = (booleanListToInteger h,rest)
  where (h,rest) = splitAt length xs
 
-integerChunks n = unfoldr (\xs -> case xs of [] -> Nothing ; _ -> Just (takeIntegerFromBooleanList n xs))
+integerChunks n xs = unfoldr (\xs -> case xs of [] -> Nothing ; _ -> Just (takeIntegerFromBooleanList n xs)) xs
 
-word8Chunks =  map (fromIntegral :: Int -> Word8) . integerChunks 8 
-
-booleanListToByteString = Data.ByteString.pack . word8Chunks
-
-byteStringToBooleanList = listOfIntegersToListToBooleanList . Data.ByteString.unpack
+int8Chunks xs = integerChunks 8 xs
+word8Chunks xs =  map (fromIntegral :: Integral a => a -> Word8) . int8Chunks $ xs
 
 listOfIntegersToListToBooleanList = concatMap integerToBooleanList
 
+overlayRight xs ys = reverse . map head . transpose . map reverse $ [ys,xs]
 
+integerToBooleanListPadded p x = overlayRight (replicate p False) (integerToBooleanList  x)
+
+integersToPaddedBooleansLists p xs = map (integerToBooleanListPadded p) xs
+
+integersToPaddedBooleans p xs = concat (integersToPaddedBooleansLists p xs)
+
+toBoolean8s xs = integersToPaddedBooleans 8 xs
+
+booleanListToByteString = Data.ByteString.pack . word8Chunks
+byteStringToBooleanList = listOfIntegersToListToBooleanList . Data.ByteString.unpack
--- Tests.hs
+++ Tests.hs
@@ -1,5 +1,7 @@
 import Data.BooleanList
 import Test.HUnit
+import qualified Data.ByteString as BS
+import Data.Char
 
 makeCase x y = TestCase (assertEqual "" x y)
 
@@ -8,5 +10,8 @@ main = runTestTT $ TestList [
     ,makeCase (booleanListToInteger [True,True,False,False]) 12
     ,makeCase (byteStringToBooleanList $ booleanListToByteString [True,True,False,False]) ([True,True,False,False])
     ]
+
+-- f :: BS.ByteString
+f = BS.pack $ map (fromIntegral . ord) $ "The rain in spain falls mainly on the plane"
 
 
